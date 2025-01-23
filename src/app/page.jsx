41ae67a3a668fda624843a24a13d1922f52f5cfd
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2025-01-23 23:42:29 +0600

    vercel deploy added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ const Home = async () => {
   const blogs = await response.json()
   return (
     <div className="my-3 py-3 ">
-      <h3 classNamen="text-center md:text-4xl bg-gradient-to-r from-purple-700 to-blue-700 bg-clip-text  text-2xl text-transparent leading-normal">Here is the All Blogs Title</h3>
+      <h3 className="text-center md:text-4xl text-2xl bg-gradient-to-r from-purple-700 to-blue-700 bg-clip-text text-transparent leading-normal">Here is the All Blogs Title</h3>
       <ul className="md:px-12 py-12">
         {
           blogs.map(blog => <li className="text-xl hover:underline py-2 hover:text-green-800" key={blog.id}>
