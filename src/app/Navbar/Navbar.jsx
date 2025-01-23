e307442cb6774b8fcc3f650a534a263a2e0d3e0c
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2025-01-23 18:03:28 +0600

    google user login logout added

--- src/app/Navbar/Navbar.jsx
+++ src/app/Navbar/Navbar.jsx
@@ -1,15 +1,23 @@
 import Link from 'next/link';
-import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const Navbar = () => { 
+const Navbar = async () => {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
     return (
         <div className='container mx-auto py-4 border-b shadow-sm'>
             <ul className='flex  items-center justify-center gap-5'>
                 <li><Link className='hover:text-green-500' href='/'>Home</Link></li>
                 <li><Link className='hover:text-green-500' href='/profile'>Profile</Link></li>
+                {user ?
+                    <LogoutLink className='hover:text-green-500'>Log out</LogoutLink>
+                    : <>
+                        <LoginLink className='hover:text-green-500'>Sign in</LoginLink>
+                        <RegisterLink className='hover:text-green-500'>Sign up</RegisterLink>
+                    </>
+                }
 
-                <LoginLink>Sign in</LoginLink>
-                <RegisterLink>Sign up</RegisterLink>
             </ul>
         </div>
     );
